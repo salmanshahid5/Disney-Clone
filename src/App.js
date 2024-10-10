cd743c744f27f571f18d61acc8506327f7be2b45
salmanshahid5 <salmanshahid0333@gmail.com>
2024-10-10 16:36:32 +0500

    header design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+import MyState from "./components/context/MyState";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <MyState>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+    </MyState>
   );
 }
 
